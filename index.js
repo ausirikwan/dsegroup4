1cfb13b92a2216f16beb46dce3f48bac0b99ee49
ausirikwan <86187201+ausirikwan@users.noreply.github.com>
2021-06-22 19:09:20 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send("hello")
+})
+
 app.get('/testapi', (req, res) => {
   res.json({"detail":"Hello world"})
 })
